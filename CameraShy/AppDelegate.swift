21dac9e75dcbb1f75834ac11563491a8a8d955de
Elad Dekel <eladdekel2@gmail.com>
2021-02-21 07:45:34 -0500

    Final Commit

--- CameraShy/AppDelegate.swift
+++ CameraShy/AppDelegate.swift
@@ -36,6 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // OneSignal initialization
         OneSignal.initWithLaunchOptions(launchOptions)
+       
         
         
         
